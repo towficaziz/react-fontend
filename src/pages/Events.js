a4f4101961e6bc7fd3f69fa8143d5744040bf981
towficaziz <tapolly01@gmail.com>
2023-09-23 01:04:15 +0200

    navigate to active classes and add a link to respective eventpage

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,5 +1,46 @@
-function EventsPage(){
-    return <h1>EventsPage</h1>;
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event1",
+  },
+  {
+    id: "e2",
+    title: "Some event2",
+  },
+  {
+    id: "e3",
+    title: "Some event3",
+  },
+  {
+    id: "e4",
+    title: "Some event4",
+  },
+  {
+    id: "e5",
+    title: "Some event5",
+  },
+  {
+    id: "e6",
+    title: "Some event6",
+  },
+];
+function EventsPage() {
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map(
+          (event) => (
+            <li key={event.id}>
+              <Link to={event.id}>{event.title}</Link>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
