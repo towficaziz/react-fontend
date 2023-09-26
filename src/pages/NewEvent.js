d25adb0bf51ed955cbc6704b318ec6fbf167c321
towficaziz <tapolly01@gmail.com>
2023-09-27 00:30:57 +0200

    The useRouteLoaderData()Hook and Accessing Data From other Routes

--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -1,5 +1,8 @@
+import EventForm from "../components/EventForm";
+
 function NewEventPage(){
-    return <h1>NewEventPagePage</h1>;
+    
+    return <EventForm />;
 }
 
 export default NewEventPage;
